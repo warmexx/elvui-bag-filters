21ca34e818e513f7dd011cd8a83015a47fb32447
Warmexx <warmexx@wow-neighbours.com>
2024-07-27 11:35:18 +0200

    Renames FilterHolder because someone else decided to duplicate

--- Core/BagFilter.lua
+++ Core/BagFilter.lua
@@ -9,13 +9,13 @@ local GetItemInfo = C_Item.GetItemInfo or GetItemInfo;
 
 local function SetSlotFilter(self, bagID, slotID)
     local f = B:GetContainerFrame(bagID > (NUM_TOTAL_EQUIPPED_BAG_SLOTS or NUM_BAG_SLOTS) or bagID == BANK_CONTAINER);
-    if not (f and f.FilterHolder) then return end
+    if not (f and f.BagFilter) then return end
 
-    if f.FilterHolder.active and self.Bags[bagID] and self.Bags[bagID][slotID] then
+    if f.BagFilter.active and self.Bags[bagID] and self.Bags[bagID][slotID] then
         local location = { bagID = bagID, slotIndex = slotID };
         local link;
         link = C_Container.GetContainerItemLink(bagID, slotID);
-        if not link or f.FilterHolder[f.FilterHolder.active].filter(location, link, select(12, GetItemInfo(link))) then
+        if not link or f.BagFilter[f.BagFilter.active].filter(location, link, select(12, GetItemInfo(link))) then
             self.Bags[bagID][slotID].searchOverlay:Hide();
         else
             self.Bags[bagID][slotID].searchOverlay:Show();
@@ -25,14 +25,14 @@ end
 
 local function SetFilter(self)
     local f = B:GetContainerFrame(self.isBank);
-    if not (f and f.FilterHolder) then return end
+    if not (f and f.BagFilter) then return end
 
     for i = 1, U.numFilters do
         if i ~= self:GetID() then
-            f.FilterHolder[i]:SetChecked(nil);
+            f.BagFilter[i]:SetChecked(nil);
         end
     end
-    f.FilterHolder.active = self:GetID();
+    f.BagFilter.active = self:GetID();
 
     for i, bagID in ipairs(f.BagIDs) do
         if f.Bags[bagID] then
@@ -45,11 +45,11 @@ end
 
 local function ResetFilter(self)
     local f = B:GetContainerFrame(self.isBank);
-    if not (f and f.FilterHolder) then return end
+    if not (f and f.BagFilter) then return end
 
-    if f.FilterHolder.active then
-        f.FilterHolder[f.FilterHolder.active]:SetChecked(nil);
-        f.FilterHolder.active = nil;
+    if f.BagFilter.active then
+        f.BagFilter[f.BagFilter.active]:SetChecked(nil);
+        f.BagFilter.active = nil;
 
         for i, bagID in ipairs(f.BagIDs) do
             if f.Bags[bagID] then
@@ -69,44 +69,44 @@ local function AddFilterButtons(f, isBank)
     local lastContainerButton;
 
     for i, filter in ipairs(U.Filters) do
-        if not f.FilterHolder[i] then
+        if not f.BagFilter[i] then
             local name, icon, func = unpack(filter);
 
             if E.Classic then
-                f.FilterHolder[i] = CreateFrame('CheckButton', nil, f.FilterHolder);
+                f.BagFilter[i] = CreateFrame('CheckButton', nil, f.BagFilter);
             else
-                f.FilterHolder[i] = CreateFrame('CheckButton', nil, f.FilterHolder, 'BackdropTemplate');
+                f.BagFilter[i] = CreateFrame('CheckButton', nil, f.BagFilter, 'BackdropTemplate');
             end
-            f.FilterHolder[i]:SetTemplate('Default', true);
-            f.FilterHolder[i]:StyleButton();
-            f.FilterHolder[i]:SetNormalTexture('');
-            f.FilterHolder[i]:SetPushedTexture('');
-            f.FilterHolder[i].ttText = name;
-            f.FilterHolder[i].filter = func;
-            f.FilterHolder[i].isBank = isBank;
-            f.FilterHolder[i]:SetScript('OnEnter', B.Tooltip_Show);
-            f.FilterHolder[i]:SetScript('OnLeave', B.Tooltip_Hide);
-            f.FilterHolder[i]:SetScript('OnClick', SetFilter);
-            f.FilterHolder[i]:SetScript('OnHide', ResetFilter);
-            f.FilterHolder[i]:SetID(i);
+            f.BagFilter[i]:SetTemplate('Default', true);
+            f.BagFilter[i]:StyleButton();
+            f.BagFilter[i]:SetNormalTexture('');
+            f.BagFilter[i]:SetPushedTexture('');
+            f.BagFilter[i].ttText = name;
+            f.BagFilter[i].filter = func;
+            f.BagFilter[i].isBank = isBank;
+            f.BagFilter[i]:SetScript('OnEnter', B.Tooltip_Show);
+            f.BagFilter[i]:SetScript('OnLeave', B.Tooltip_Hide);
+            f.BagFilter[i]:SetScript('OnClick', SetFilter);
+            f.BagFilter[i]:SetScript('OnHide', ResetFilter);
+            f.BagFilter[i]:SetID(i);
 
-            f.FilterHolder[i].iconTexture = f.FilterHolder[i]:CreateTexture();
-            f.FilterHolder[i].iconTexture:SetInside();
-            f.FilterHolder[i].iconTexture:SetTexCoord(unpack(E.TexCoords));
-            f.FilterHolder[i].iconTexture:SetTexture(icon);
+            f.BagFilter[i].iconTexture = f.BagFilter[i]:CreateTexture();
+            f.BagFilter[i].iconTexture:SetInside();
+            f.BagFilter[i].iconTexture:SetTexCoord(unpack(E.TexCoords));
+            f.BagFilter[i].iconTexture:SetTexture(icon);
         end
 
-        f.FilterHolder:Size(((buttonSize + buttonSpacing) * i) + buttonSpacing, buttonSize + (buttonSpacing * 2));
+        f.BagFilter:Size(((buttonSize + buttonSpacing) * i) + buttonSpacing, buttonSize + (buttonSpacing * 2));
 
-        f.FilterHolder[i]:Size(buttonSize);
-        f.FilterHolder[i]:ClearAllPoints();
+        f.BagFilter[i]:Size(buttonSize);
+        f.BagFilter[i]:ClearAllPoints();
         if i == 1 then
-            f.FilterHolder[i]:SetPoint('BOTTOMLEFT', f.FilterHolder, 'BOTTOMLEFT', buttonSpacing, buttonSpacing)
+            f.BagFilter[i]:SetPoint('BOTTOMLEFT', f.BagFilter, 'BOTTOMLEFT', buttonSpacing, buttonSpacing)
         else
-            f.FilterHolder[i]:SetPoint('LEFT', lastContainerButton, 'RIGHT', buttonSpacing, 0);
+            f.BagFilter[i]:SetPoint('LEFT', lastContainerButton, 'RIGHT', buttonSpacing, 0);
         end
 
-        lastContainerButton = f.FilterHolder[i];
+        lastContainerButton = f.BagFilter[i];
     end
 end
 
@@ -114,15 +114,15 @@ local function AddMenuButton(isBank)
     if E.private.bags.enable ~= true then return end
     local f = B:GetContainerFrame(isBank);
 
-    if not f or f.FilterHolder then return end
+    if not f or f.BagFilter then return end
     if E.Classic then
-        f.FilterHolder = CreateFrame('Button', nil, f);
+        f.BagFilter = CreateFrame('Button', nil, f);
     else
-        f.FilterHolder = CreateFrame('Button', nil, f, 'BackdropTemplate');
+        f.BagFilter = CreateFrame('Button', nil, f, 'BackdropTemplate');
     end
-    f.FilterHolder:Point('BOTTOMLEFT', f, 'TOPLEFT', 0, 1);
-    f.FilterHolder:SetTemplate('Transparent');
-    f.FilterHolder:Hide();
+    f.BagFilter:Point('BOTTOMLEFT', f, 'TOPLEFT', 0, 1);
+    f.BagFilter:SetTemplate('Transparent');
+    f.BagFilter:Hide();
 
     if E.Classic then
         f.filterButton = CreateFrame('Button', nil, f.holderFrame);
@@ -144,11 +144,11 @@ local function AddMenuButton(isBank)
     f.filterButton:SetScript('OnLeave', B.Tooltip_Hide);
     f.filterButton:SetScript('OnClick', function()
         f.ContainerHolder:Hide();
-        ToggleFrame(f.FilterHolder);
+        ToggleFrame(f.BagFilter);
     end);
 
     f.bagsButton:HookScript('OnClick', function()
-        f.FilterHolder:Hide();
+        f.BagFilter:Hide();
     end);
 
     -- realign
